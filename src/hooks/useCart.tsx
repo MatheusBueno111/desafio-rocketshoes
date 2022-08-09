013367c3d430e84b95a322287ba6965ee43036bb
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2022-08-09 19:42:30 -0300

    finalizando

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { Product } from '../types';
 
 interface CartProviderProps {
   children: ReactNode;
@@ -14,9 +14,9 @@ interface UpdateProductAmount {
 
 interface CartContextData {
   cart: Product[];
+  updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
   addProduct: (productId: number) => Promise<void>;
   removeProduct: (productId: number) => void;
-  updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
 }
 
 const CartContext = createContext<CartContextData>({} as CartContextData);
@@ -34,17 +34,49 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+      const response = await api.get(`/products/${productId}`)
+      const product = response.data 
+
+      const updatedCart = [...cart] //a lista mais att do cart possivel
+      const productExists = updatedCart.find((product) => product.id===productId)// so vai ter quando o produto for add mais de 1x
+      console.log('productExists :>>', productExists) 
+
+      const responseAmount =await api.get(`/stock/${productId}`)
+      const productStock = responseAmount.data.amount //stock(amount) do produto
+      console.log('productStock :>>', productStock) 
+      
+      if(productExists) { //caso exista
+        if(productStock > productExists.amount) { //conferir sua quantidade no estoque
+          productExists.amount = productExists.amount + 1 
+        } else {
+          toast.error('Quantidade solicitada fora de estoque');
+        }
+      } else {
+          const newProduct = {
+            ...product,
+            amount: 1
+          }
+        setCart([...cart, newProduct])
+      }
+       
+    } catch (error){
+      toast.error('Erro na adição do produto');
     }
   };
+  
+  useEffect(()=> {
+    console.log('cart :>>', cart)
+  },[cart])
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart]//a lista mais att do cart possivel
+      const newCart = updatedCart.filter((product) => { 
+        return product.id !== productId //retorna todos menos o productId
+      })
+      setCart(newCart)
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
@@ -53,9 +85,24 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      const updatedCart = [...cart]// a lista mais att do cart possivel
+      const productExists = updatedCart.find((product) => product.id===productId)
+      
+      const responseAmount =await api.get(`/stock/${productId}`)
+      const productStock = responseAmount.data.amount
+
+      console.log('amount', amount)
+      if(productExists){
+        if(productStock >= amount && productExists.amount > 0) {
+          
+          productExists.amount = amount       
+        } else {
+          toast.error('Quantidade solicitada fora de estoque');
+        }
+        setCart(updatedCart)
+      }
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
